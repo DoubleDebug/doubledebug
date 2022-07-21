6aa3052adfd1ecdb5e5d92471268572005f3f476
DoubleDebug <duledodicc@gmail.com>
2022-07-21 15:28:42 +0200

    chore: open graph info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,39 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ChakraProvider>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        {/* HTML Meta Tags */}
+        <meta
+          name="description"
+          content="Get to know me, explore my projects and read my blog | Web developer portfolio website"
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://doubledebug.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Double Debug | Full stack web developer"
+        />
+        <meta
+          property="og:description"
+          content="Get to know me, explore my projects and read my blog | Web developer portfolio website"
+        />
+        <meta property="og:image" content="images/og-image.png" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="doubledebug.vercel.app" />
+        <meta property="twitter:url" content="https://doubledebug.vercel.app" />
+        <meta
+          name="twitter:title"
+          content="Double Debug | Full stack web developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Get to know me, explore my projects and read my blog | Web developer portfolio website"
+        />
+        <meta name="twitter:image" content="images/og-image.png" />
       </Head>
       <Nav />
       <Component {...pageProps} />
